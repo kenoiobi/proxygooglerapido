953e85591e4f682cca4c15aad6ec4492352e650a
kenoiobi <kenoiobi@gmail.com>
2023-06-14 10:29:28 -0300

    Update comando.sh

--- comando.sh
+++ comando.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-sudo -i
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt update
-sudo apt install nano
+sudo apt install -y nano
 wget http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb
-sudo apt install gdebi-core
+sudo apt install -y gdebi-core
