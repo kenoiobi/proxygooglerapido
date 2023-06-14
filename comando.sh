151a3bc58a60ecb30cbb1a5930a1bacd76bfe42c
kenoiobi <kenoiobi@gmail.com>
2023-06-14 10:53:48 -0300

    Update comando.sh

--- comando.sh
+++ comando.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt update
-sudo apt install nano
 wget https://github.com/kenoiobi/proxygooglerapido/blob/main/dante-server_1.4.1-1_amd64.deb
 sudo apt install gdebi-core
 gdebi dante-server_1.4.1-1_amd64.deb
