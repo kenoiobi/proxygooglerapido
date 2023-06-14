dcf421767cbed03dc50407ae9e630fec0df14e10
kenoiobi <kenoiobi@gmail.com>
2023-06-14 10:37:13 -0300

    Update comando.sh

--- comando.sh
+++ comando.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget --debug http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb
+wget 'http://ppa.launchpad.net/dajhorn/dante/ubuntu/pool/main/d/dante/dante-server_1.4.1-1_amd64.deb'
